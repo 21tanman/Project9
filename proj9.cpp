d97c038d1105adcee932c0f5451b39223bbdd187
Tanner <tannerafranz@gmail.com>
2019-03-24 15:15:44 -0700

    Trying to add to git without created repo first

---
+++ proj9.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+#include "proj9.h"
+
+
+List4::List4()
+	{
+		head = nullptr;
+		tail = nullptr;
+	}
+
+List4:: ~List4()
+	{
+//LEARN DESTRUCTOR
+	}
