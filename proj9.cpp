a5c67e709c7ce1c525cc03f8acce780ba3cd4264
Tanner <tannerafranz@gmail.com>
2019-03-24 15:43:34 -0700

    start of project 9

--- proj9.cpp
+++ proj9.cpp
@@ -15,3 +15,21 @@ List4:: ~List4()
 	{
 //LEARN DESTRUCTOR
 	}
+doubleNode* FindPosition(int pos)
+	{
+		doubleNode* cur = new node;
+		cur->next = head;
+		for (int i = 0; i< pos; i++)
+			{
+				cur = cur->next;
+			}
+		return cur;
+	}
+
+void Insert (itemType item, int pos)
+	{
+		doubleNode* cur = new node;
+		cur->next = FindPosition(pos);
+		cur-> prev = 
+		 
+						
