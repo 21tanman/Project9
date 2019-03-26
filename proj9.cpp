5b72fd08454dfd2179130c6b95c70f69c0667a27
Tanner <tannerafranz@gmail.com>
2019-03-25 20:06:03 -0700

    Added some more to insert function

--- proj9.cpp
+++ proj9.cpp
@@ -13,13 +13,14 @@ List4::List4()
 
 List4:: ~List4()
 	{
-//LEARN DESTRUCTOR
+		delete head;
+		delete tail;
 	}
 doubleNode* FindPosition(int pos)
 	{
 		doubleNode* cur = new node;
-		cur->next = head;
-		for (int i = 0; i< pos; i++)
+		cur = head;
+		for (int i = 0; i < pos; i++)
 			{
 				cur = cur->next;
 			}
@@ -28,8 +29,17 @@ doubleNode* FindPosition(int pos)
 
 void Insert (itemType item, int pos)
 	{
+		
 		doubleNode* cur = new node;
-		cur->next = FindPosition(pos);
-		cur-> prev = 
-		 
+		doubleNode* pre = new node;
+		doubleNode* post = new node;
+		post = FindPosition(pos);
+		pre = FindPosition(pos-1)
+		pre-> next = cur;
+		post-> prev = cur;
+		cur-> item = item;
+		cur-> next = post;
+		cur-> prev = pre;
+		length++;
+	}
 						
